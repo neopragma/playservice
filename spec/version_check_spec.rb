4beaa09a4aec94256b8a4e1a48ce626dca8fc1f9
davenicolette <davenicolette@gmail.com>
2018-02-12 16:29:27 -0500

    Fixed URL in version_check_spec.rb

--- spec/version_check_spec.rb
+++ spec/version_check_spec.rb
@@ -5,7 +5,7 @@ require 'rest-client'
 describe 'playservice: ' do
   context 'verifying version support: ' do 
     it 'says that it supports version 1.0.0', :integration => true do
-      response = RestClient.get "#{ENV['PLAY_URL']}/v1.0.0/"
+      response = RestClient.get "#{ENV['PLAY_URL']}/v1.0.0"
       expect(JSON.parse(response)['service'])
         .to eq('playservice')
     end
